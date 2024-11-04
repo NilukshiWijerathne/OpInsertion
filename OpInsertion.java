0440c6339129e233ad1c8b3871fcd37686ad5d46
NilukshiWijerathne <nilukshiwijerathne2005@gmail.com>
2024-11-04 14:12:48 +0530

    Add files via upload

---
+++ OpInsertion.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraytest;
+
+
+import java.util.Arrays;
+
+public class OpInsertion {
+
+    static Integer[] array = {76,6,107,92,21,23,5,9,8,1843};
+    static int outerIndex;
+    static int temp;
+    
+    public static int shift(int startIndex){
+        temp = array [startIndex];
+        int i = startIndex;
+
+    for (; i>0 && array [i-1] > temp; i--){
+        array[i] = array[i-1];
+        System.out.println("i: " + i + ", j: " + (i-1) + ", " + "Array State: " + Arrays.deepToString(array));
+}
+   return i;    
+         
+}
+    public static void insert(int endIndex ){
+        int position = shift(endIndex);
+ 
+        array[position] = temp;
+
+        System.out.println("After i = " + outerIndex + ";" + Arrays.deepToString(array));
+
+          
+}
+
+    public static void insertionSort(){
+         for(outerIndex = 1; outerIndex < array.length; outerIndex++){
+              insert(outerIndex);
+       }
+}           
+
+
+    
+    public static void main(String[] args) {
+       System.out.println("Arrays before Sorting: " + Arrays.deepToString(array));
+		
+		insertionSort();
+		
+		System.out.println("Array after Sorting: " + Arrays.deepToString(array));
+		
+         }		
+}
+
+
